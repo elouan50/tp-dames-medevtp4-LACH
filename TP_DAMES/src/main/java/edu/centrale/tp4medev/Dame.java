e07014a422120b20f6737ef1312641ca2d0a5dfa
elouan50 <elouan.colybes@gmail.com>
2022-12-15 12:18:40 +0100

    fix utilisation tableau

--- TP_DAMES/src/main/java/edu/centrale/tp4medev/Dame.java
+++ TP_DAMES/src/main/java/edu/centrale/tp4medev/Dame.java
@@ -6,47 +6,59 @@ public class Dame {
     }
 
     /**
-     * Constructeur d'une nouvelle Dame (pour la couleur : 1 = blanc, 0 = noir)
+     * Constructeur d'une nouvelle Dame (pour la couleur : 1 = blanc, 0 = noir).
+     * @param couleur Couleur de la dame créée (1 = blanc, 0 = noir)
      */
     public Dame(boolean couleur) {
         super(couleur);
     }
 
-    public boolean deplace_possible(Case i, Case f, Plateau p) {
+    /**
+     * Teste si le déplacement d'une case à une autre est possible, pour une dame.
+     * @param i Case du pion qu'on cherche à déplacer.
+     * @param f Case où on cherche à déplacer le pion.
+     * @param p Plateau sur lequel on évolue.
+     * @return Vrai si le déplacement est possible, faux sinon.
+     */
+    public boolean deplace_possible(Case i, Case f, Plateau pla) {
+
+        List<List<Case>> p = pla.getPlateau();
+
         if (f.getPion() == null) {
             if (i.getX()+i.getY() == f.getX()+f.getY()) {
                 // Cas d'une diagonale /
                 if (i.getX()<=f.getX()) {
                     for (int j=i.getX()+1 ; j<f.getX() ; j++) {
-                        if (p[j][i.getX()+i.getY()-j].getPion() != null) {
-                            if (p[j][i.getX()+i.getY()-j].getPion().getCouleur()==i.getPion().getCouleur()) {
+                        if (p.get(j).get(i.getX()+i.getY()-j).getPion() != null) {
+                            if (p.get(j).get(i.getX()+i.getY()-j).getPion().getCouleur()==i.getPion().getCouleur()) {
                                 return false;
                             }
                         }
                     }
                 } else {
                     for (int j=f.getX()+1 ; j<i.getX() ; j++) {
-                        if (p[j][i.getX()+i.getY()-j].getPion() != null) {
-                            if (p[j][i.getX()+i.getY()-j].getPion().getCouleur()==i.getPion().getCouleur()) {
+                        if (p.get(j).get(i.getX()+i.getY()-j).getPion() != null) {
+                            if (p.get(j).get(i.getX()+i.getY()-j).getPion().getCouleur()==i.getPion().getCouleur()) {
                                 return false;
                             }
                         }
                     }
                 }
+                return false;
             } else if(i.getX()-i.getY() == f.getX()-f.getY()){
                 // Cas d'une diagonale \
                 if (i.getX()<=f.getX()) {
                     for (int j=i.getX()+1 ; j<f.getX() ; j++) {
-                        if (p[j][j-i.getX()+i.getY()].getPion() != null) {
-                            if (p[j][j-i.getX()+i.getY()].getPion().getCouleur()==i.getPion().getCouleur()) {
+                        if (p.get(j).get(j-i.getX()+i.getY()).getPion() != null) {
+                            if (p.get(j).get(j-i.getX()+i.getY()).getPion().getCouleur()==i.getPion().getCouleur()) {
                                 return false;
                             }
                         }
                     }
                 } else {
                     for (int j=f.getX()+1 ; j<i.getX() ; j++) {
-                        if (p[j][j-i.getX()+i.getY()].getPion() != null) {
-                            if (p[j][j-i.getX()+i.getY()].getPion().getCouleur()==i.getPion().getCouleur()) {
+                        if (p.get(j).get(j-i.getX()+i.getY()).getPion() != null) {
+                            if (p.get(j).get(j-i.getX()+i.getY()).getPion().getCouleur()==i.getPion().getCouleur()) {
                                 return false;
                             }
                         }
@@ -57,7 +69,10 @@ public class Dame {
         }
     }
 
-
+    /**
+     * Permet de manger le pion de la case passée en paramètre.
+     * @param c Case sur laquelle on passe manger (qu'il y ait un pion ou pas).
+     */
     @Override
     public void manger(Case c) {
         if (c.getPion() != null){
@@ -65,29 +80,38 @@ public class Dame {
         }
     }
 
+    /**
+     * Déplace le pion d'une case donnée vers une autre case.
+     * @param i Case du pion qu'on cherche à déplacer.
+     * @param f Case où on cherche à déplacer le pion.
+     * @param p Plateau sur lequel on évolue.
+     */
     @Override
-    public void deplacer(Case i, Case f, Plateau p) {
+    public void deplacer(Case i, Case f, Plateau pla) {
+
+        List<List<Case>> p = pla.getPlateau();
+
         if (deplace_possible(i,f,p)) {
             if (i.getX()+i.getY() == f.getX()+f.getY()) {
                 // Cas d'une diagonale /
                 if (i.getX()<=f.getX()) {
                     for (int j=i.getX()+1 ; j<f.getX() ; j++) {
-                        this.manger(p[j][i.getX()+i.getY()-j]);
+                        this.manger(p.get(j).get(i.getX()+i.getY()-j));
                     }
                 } else {
                     for (int j=f.getX()+1 ; j<i.getX() ; j++) {
-                        this.manger(p[j][i.getX()+i.getY()-j]);
+                        this.manger(p.get(j).get(i.getX()+i.getY()-j));
                     }
                 }
             } else if(i.getX()-i.getY() == f.getX()-f.getY()){
                 // Cas d'une diagonale \
                 if (i.getX()<=f.getX()) {
                     for (int j=i.getX()+1 ; j<f.getX() ; j++) {
-                        this.manger(p[j][j-i.getX()+i.getY()]);
+                        this.manger(p.get(j).get(j-i.getX()+i.getY()));
                     }
                 } else {
                     for (int j=f.getX()+1 ; j<i.getX() ; j++) {
-                        this.manger(p[j][j-i.getX()+i.getY()]);
+                        this.manger(p.get(j).get(j-i.getX()+i.getY()));
                     }
                 }
             }
