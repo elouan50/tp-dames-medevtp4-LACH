e8ddc35181403e5cb81d5d54dc22c148885c6003
Elisa <elisa.andrianarison@eleves.ec-nantes.fr>
2022-12-15 11:08:04 +0100

    .

--- TP_DAMES/src/main/java/edu/centrale/tp4medev/Case.java
+++ TP_DAMES/src/main/java/edu/centrale/tp4medev/Case.java
@@ -6,13 +6,27 @@ public class Case {
     private int y;
     private Pion p;
 
-    public Case(int x, int y, Pion p){
+    /**
+     * 
+     */
+    public Case() {
+        x = 0;
+        y = 0;
+        p = null;
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @param p
+     */
+    public Case(int x, int y, Pion p) {
         this.x = x ;
         this.y = y ;
         this.p = p ;
     }
 
-    public boolean jouable(){
+    public boolean jouable() {
         boolean jouable;
         if ((x+y)%2==0){
             jouable = false;
